7351a787039a77c653059770aa842a28d5c455a0
ArticPolarDEV <vilma4040p@gmail.com>
2023-09-19 18:31:43 +0000

    Source Code
    Add Source Code of Lib and Example in Repository

---
+++ ArticPolar.Dev.FetchData/FetchData.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ArticPolar.Dev.FetchData
+{
+    public class FetchData
+    {
+        private readonly HttpClient httpClient;
+
+        public FetchData()
+        {
+            httpClient = new HttpClient();
+        }
+
+        public string Fetch(string apiUrl)
+        {
+            try
+            {
+                HttpResponseMessage response = httpClient.GetAsync(apiUrl).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string data = response.Content.ReadAsStringAsync().Result;
+                    return data;
+                }
+                else
+                {
+                    // Em caso de erro, você pode tratar aqui de acordo com suas necessidades.
+                    MessageBox.Show($"Erro na solicitação HTTP GET: {response.StatusCode}");
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Trate exceções, se necessário.
+                MessageBox.Show($"Erro na solicitação HTTP GET: {ex.Message}");
+                return null;
+            }
+        }
+
+        public string PostData(string apiUrl, string content)
+        {
+            try
+            {
+                var httpContent = new StringContent(content, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = httpClient.PostAsync(apiUrl, httpContent).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseData = response.Content.ReadAsStringAsync().Result;
+                    return responseData;
+                }
+                else
+                {
+                    // Em caso de erro, você pode tratar aqui de acordo com suas necessidades.
+                    Console.WriteLine($"Erro na solicitação HTTP POST: {response.StatusCode}");
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Trate exceções, se necessário.
+                Console.WriteLine($"Erro na solicitação HTTP POST: {ex.Message}");
+                return null;
+            }
+        }
+    }
+}
---
+++ FetchConsoleTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("FetchConsoleTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FetchConsoleTest")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("5b8c8748-2113-4b57-aa12-ec4f8938c053")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
